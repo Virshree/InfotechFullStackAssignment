c717af7d4f4ff5fdfc7e9f9afcbcaba9e34371d4
Virshree <virshreedesai19@gmail.com>
2025-10-15 13:45:07 +0530

    added files

---
+++ script.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector(".menu");
+const navItems = document.querySelector(".nav-items");
+const closeBtn = document.querySelector(".close-btn");
+const logo=document.querySelector(".logo");
+const overlay=document.querySelector(".overlay");
+const contactBtn=document.querySelector(".contact-btn")
+// open menu
+menu.addEventListener("click", function () {
+  navItems.style.display = "flex";
+  closeBtn.style.display = "block";
+  logo.style.display="block";
+  overlay.style.display="block";
+    contactBtn.style.display="block";
+});
+
+// close menu
+closeBtn.addEventListener("click", function () {
+  navItems.style.display = "none";
+  closeBtn.style.display = "none";
+  overlay.style.display="none";
+  contactBtn.style.display="none";
+//   logo.style.display="none";
+});
